7600a4b297828284fe106b124772c1e1ab4671f8
NanteRanaivosoa <rnantenainamaminirina@gmail.com>
2025-04-25 09:36:56 +0300

    add script page to update

---
+++ scriptUpdates.js
@@ -0,0 +1,14 @@
+// Animation fade-in au scroll
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOnScroll = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.1 });
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
